426343b80d669a586648102b042a8216dd06fd34
l00870118 <luxinbo4@huawei.com>
2024-12-27 10:57:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# CSV-Manipulate
+
+Get/Set a cell's value in a csv file using row/column names. For now, only separator ',' is supported.
+
+# Demo
+
+![demo](demo.gif)
+
+# Usage
+`ctrl+shift+p` and type `get cell` or `set cell`. You will be prompted to input `row-name, col-name [,target-value]`. Note that the row name, col name, target value will be trimmed first.
---
+++ src/extension.ts
@@ -0,0 +1,119 @@
+import * as vscode from 'vscode';
+
+function parseSetInput(input: string): { row: string, col: string, target: string } {
+	const [row, col, target] = input.split(',');
+	return {
+		row: row.trim(),
+		col: col.trim(),
+		target: target.trim()
+	};
+}
+
+function parseGetInput(input: string): { row: string, col: string } {
+	const [row, col] = input.split(',');
+	return {
+		row: row.trim(),
+		col: col.trim()
+	};
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	const setCellValueDisposable = vscode.commands.registerCommand('extension.setCell', async function () {
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const document = editor.document;
+
+			const userResponse = await vscode.window.showInputBox({
+				placeHolder: 'row name, col name, cell value'
+			});
+
+			if (!userResponse) {
+				return;
+			}
+
+			/* input looks like: "libwireguard.so, some_random_col_name, Y"
+			 * meaning: row, col, target cell value
+			*/
+			const { row, col, target } = parseSetInput(userResponse);
+			/* search the currently opened csv file, find the target cell, and set the value */
+			const colNumber = document.lineAt(0).text.split(',').indexOf(col);
+			if (colNumber === -1) {
+				vscode.window.showErrorMessage(`col: ${col} not found`);
+				return;
+			}
+			/* then get row number */
+			let rowNumber = -1;
+			document.getText().split('\n').find((line, index) => {
+				const firstComma = line.indexOf(',');
+				if (firstComma !== -1 && line.substring(0, firstComma) === row) {
+					rowNumber = index;
+					return true;
+				}
+			});
+			if (rowNumber === -1) {
+				vscode.window.showErrorMessage(`row: ${row} not found`);
+				return;
+			}
+
+			let cellVal = '';
+			let charCnt = 0;
+			const targetRow = document.lineAt(rowNumber);
+			/* find the cell start */
+			targetRow.text.split(',').find((cell, index) => {
+				if (index === colNumber) {
+					cellVal = cell;
+					return true;
+				}
+				charCnt += cell.length + 1; /* +1 for the comma */
+			});
+
+			const targetCellStart = targetRow.range.start.translate({ characterDelta: charCnt });
+			const targetCellEnd = targetCellStart.translate({ characterDelta: cellVal.length  });
+			const selection = new vscode.Selection(targetCellStart, targetCellEnd);
+
+			
+			await editor.edit(editBuilder => {
+				editBuilder.replace(selection, target);
+			});
+			await document.save();
+		}
+	});
+	const getCellValueDisposable = vscode.commands.registerCommand('extension.getCell', async function () {
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const document = editor.document;
+
+			const userResponse = await vscode.window.showInputBox({
+				placeHolder: 'row name, col name'
+			});
+
+			if (!userResponse) {
+				return;
+			}
+
+			/* input looks like: "libwireguard.so, some_random_col_name"
+			 * meaning: row, col
+			*/
+			const { row, col } = parseGetInput(userResponse);
+			/* search the currently opened csv file, find the target cell, and set the value */
+			
+			/* first get the col number */
+			const colNumber = document.lineAt(0).text.split(',').indexOf(col);
+			/* then get the target cell */
+			document.getText().split('\n').find((line, _) => {
+				const firstComma = line.indexOf(',');
+				if (firstComma !== -1 && line.substring(0, firstComma) === row) {
+					vscode.window.showInformationMessage(`value: ${line.split(',')[colNumber]}, row: ${row}, col: ${col}`);
+					return true;
+				}
+			});
+
+		}
+	});
+	context.subscriptions.push(setCellValueDisposable);
+	context.subscriptions.push(getCellValueDisposable);
+}
