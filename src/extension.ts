e01526bb763859b5b139931c1f2802b75e079891
l00870118 <luxinbo4@huawei.com>
2024-12-27 16:21:14 +0800

    add settings page

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,36 @@
 import * as vscode from 'vscode';
 
+const config = vscode.workspace.getConfiguration('csv-manipulate');
+const sep: string = config.get('separator') || ',';
+const trimEnabled: boolean = config.get('trimEnabled') || true;
+
 function parseSetInput(input: string): { row: string, col: string, target: string } {
-	const [row, col, target] = input.split(',');
+	const [row, col, target] = input.split(sep);
+	if (trimEnabled) {
+		return {
+			row: row.trim(),
+			col: col.trim(),
+			target: target.trim()
+		};
+	}
 	return {
-		row: row.trim(),
-		col: col.trim(),
-		target: target.trim()
+		row: row,
+		col: col,
+		target: target
 	};
 }
 
 function parseGetInput(input: string): { row: string, col: string } {
-	const [row, col] = input.split(',');
+	const [row, col] = input.split(sep);
+	if (trimEnabled) {
+		return {
+			row: row.trim(),
+			col: col.trim()
+		};
+	}
 	return {
-		row: row.trim(),
-		col: col.trim()
+		row: row,
+		col: col
 	};
 }
 
@@ -38,7 +55,7 @@ export function activate(context: vscode.ExtensionContext) {
 			*/
 			const { row, col, target } = parseSetInput(userResponse);
 			/* search the currently opened csv file, find the target cell, and set the value */
-			const colNumber = document.lineAt(0).text.split(',').indexOf(col);
+			const colNumber = document.lineAt(0).text.split(sep).indexOf(col);
 			if (colNumber === -1) {
 				vscode.window.showErrorMessage(`col: ${col} not found`);
 				return;
@@ -46,7 +63,7 @@ export function activate(context: vscode.ExtensionContext) {
 			/* then get row number */
 			let rowNumber = -1;
 			document.getText().split('\n').find((line, index) => {
-				const firstComma = line.indexOf(',');
+				const firstComma = line.indexOf(sep);
 				if (firstComma !== -1 && line.substring(0, firstComma) === row) {
 					rowNumber = index;
 					return true;
@@ -61,7 +78,7 @@ export function activate(context: vscode.ExtensionContext) {
 			let charCnt = 0;
 			const targetRow = document.lineAt(rowNumber);
 			/* find the cell start */
-			targetRow.text.split(',').find((cell, index) => {
+			targetRow.text.split(sep).find((cell, index) => {
 				if (index === colNumber) {
 					cellVal = cell;
 					return true;
@@ -70,10 +87,10 @@ export function activate(context: vscode.ExtensionContext) {
 			});
 
 			const targetCellStart = targetRow.range.start.translate({ characterDelta: charCnt });
-			const targetCellEnd = targetCellStart.translate({ characterDelta: cellVal.length  });
+			const targetCellEnd = targetCellStart.translate({ characterDelta: cellVal.length });
 			const selection = new vscode.Selection(targetCellStart, targetCellEnd);
 
-			
+
 			await editor.edit(editBuilder => {
 				editBuilder.replace(selection, target);
 			});
@@ -100,14 +117,14 @@ export function activate(context: vscode.ExtensionContext) {
 			*/
 			const { row, col } = parseGetInput(userResponse);
 			/* search the currently opened csv file, find the target cell, and set the value */
-			
+
 			/* first get the col number */
-			const colNumber = document.lineAt(0).text.split(',').indexOf(col);
+			const colNumber = document.lineAt(0).text.split(sep).indexOf(col);
 			/* then get the target cell */
 			document.getText().split('\n').find((line, _) => {
-				const firstComma = line.indexOf(',');
+				const firstComma = line.indexOf(sep);
 				if (firstComma !== -1 && line.substring(0, firstComma) === row) {
-					vscode.window.showInformationMessage(`value: ${line.split(',')[colNumber]}, row: ${row}, col: ${col}`);
+					vscode.window.showInformationMessage(`value: ${line.split(sep)[colNumber]}, row: ${row}, col: ${col}`);
 					return true;
 				}
 			});
